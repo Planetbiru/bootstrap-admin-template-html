5de64f6d9d647ed64484043a4d80e31e3f42a397
Kamshory, MT <kamshory@yahoo.com>
2024-12-28 09:47:33 +0700

    Add docblock

--- js/custom.js
+++ js/custom.js
@@ -1,54 +1,8 @@
-
-
-function initSortTable() {
-  $("table.table-sort-by-column").each(function (index, element) {
-    let thisTable = $(this);
-    let self = thisTable.attr("data-self-name");
-    let originalURL = document.location.toString();
-    let arr0 = originalURL.split("#");
-    originalURL = arr0[0];
-    let arr1 = originalURL.split("?");
-    originalURL = arr1[0];
-    let args = arr1[1] || "";
-    let argArray = args.split("&");
-    let queryObject = {};
-    for (let i in argArray) {
-      let arr2 = argArray[i].split("=");
-      if (arr2[0] != "") {
-        queryObject[arr2[0]] = arr2[1];
-      }
-    }
-    let currentOrderMethod = queryObject.ordertype || "";
-    let lastColumn = queryObject.orderby || "";
-    thisTable.find("td.order-controll").each(function (index, element) {
-      let thisCel = $(this);
-      let columnName = thisCel.attr("data-col-name");
-      if (lastColumn == columnName) {
-        if (currentOrderMethod == "asc") {
-          queryObject.ordertype = "desc";
-          thisCel.attr("data-order-method", "asc");
-        } else if (currentOrderMethod == "desc") {
-          queryObject.ordertype = "asc";
-          thisCel.attr("data-order-method", "desc");
-        } else {
-          queryObject.ordertype = "asc";
-          thisCel.attr("data-order-method", "desc");
-        }
-      } else {
-        queryObject.ordertype = "asc";
-      }
-      queryObject.orderby = columnName;
-      let arr3 = [];
-      for (let j in queryObject) {
-        arr3.push(j + "=" + queryObject[j]);
-      }
-      let args3 = arr3.join("&");
-      let finalURL = originalURL + "?" + args3;
-      thisCel.find(" > a").attr("href", finalURL);
-    });
-  });
-}
-
+/**
+ * Initializes datetime pickers for various input fields (date, time, datetime-local).
+ * - Converts the input types to text and applies respective classes.
+ * - Initializes datetime pickers with configurable min and max date options.
+ */
 function initDatetimePicker() {
   let debugDatetimePicker = false;
   
@@ -94,8 +48,8 @@ function initDatetimePicker() {
   // Initialize date-picker if there are inputs with the class 'date-picker'
   if ($('.date-picker').length) {
     $('.date-picker').each(function() {
-      var minDate = $(this).data('mindate') || false;
-      var maxDate = $(this).data('maxdate') || false;
+      let minDate = $(this).data('mindate') || false;
+      let maxDate = $(this).data('maxdate') || false;
       $(this).datetimepicker({
         minDate: minDate,
         maxDate: maxDate,
@@ -120,8 +74,8 @@ function initDatetimePicker() {
   // Initialize date-time-picker if there are inputs with the class 'date-time-picker'
   if ($('.date-time-picker').length) {
     $('.date-time-picker').each(function() {
-      var minDate = $(this).data('mindate') || false;
-      var maxDate = $(this).data('maxdate') || false;
+      let minDate = $(this).data('mindate') || false;
+      let maxDate = $(this).data('maxdate') || false;
       $(this).datetimepicker({
         minDate: minDate,
         maxDate: maxDate,
@@ -135,10 +89,83 @@ function initDatetimePicker() {
   }
 }
 
+/**
+ * Initializes table sorting functionality based on query parameters.
+ * - Allows sorting columns by clicking on headers.
+ * - Modifies the URL with updated sorting parameters.
+ */
+function initSortTable() {
+  // Select all tables with the class 'table-sort-by-column'
+  document.querySelectorAll("table.table-sort-by-column").forEach(function (thisTable) {
+    // Get the 'data-self-name' attribute of the table
+    let self = thisTable.getAttribute("data-self-name");
+    
+    // Get the current URL and process it
+    let originalURL = document.location.toString();
+    let arr0 = originalURL.split("#");
+    originalURL = arr0[0];
+    let arr1 = originalURL.split("?");
+    originalURL = arr1[0];
+    let args = arr1[1] || "";
+    let argArray = args.split("&");
+    let queryObject = {};
+
+    // Parse the query parameters into an object
+    argArray.forEach(function (param) {
+      let arr2 = param.split("=");
+      if (arr2[0] !== "") {
+        queryObject[arr2[0]] = arr2[1];
+      }
+    });
+
+    // Get the current order method and last column
+    let currentOrderMethod = queryObject.ordertype || "";
+    let lastColumn = queryObject.orderby || "";
+
+    // Find all 'td' elements with the class 'order-controll' in the table
+    thisTable.querySelectorAll("td.order-controll").forEach(function (thisCel) {
+      let columnName = thisCel.getAttribute("data-col-name");
+
+      // If this column is the last sorted column, toggle the sorting order
+      if (lastColumn === columnName) {
+        if (currentOrderMethod === "asc") {
+          queryObject.ordertype = "desc";
+          thisCel.setAttribute("data-order-method", "asc");
+        } else {
+          queryObject.ordertype = "asc";
+          thisCel.setAttribute("data-order-method", "desc");
+        }
+      } else {
+        queryObject.ordertype = "asc";
+      }
+      queryObject.orderby = columnName;
+
+      // Rebuild the query string
+      let arr3 = [];
+      for (let key in queryObject) {
+        if (queryObject.hasOwnProperty(key)) {
+          arr3.push(key + "=" + queryObject[key]);
+        }
+      }
+      let args3 = arr3.join("&");
+      let finalURL = originalURL + "?" + args3;
+
+      // Update the 'href' attribute of the link inside the cell
+      let link = thisCel.querySelector(" > a");
+      if (link) {
+        link.setAttribute("href", finalURL);
+      }
+    });
+  });
+}
+
+/**
+ * Initializes sortable rows for tables with manual sorting.
+ * - Allows drag-and-drop sorting of rows.
+ */
 function initSortData()
 {
-  $("tbody.data-table-manual-sort").each(function () {
-    let dataToSort = $(this)[0];
+  document.querySelectorAll("tbody.data-table-manual-sort").forEach(function(dataToSort) {
     Sortable.create(dataToSort, {
       animation: 150,
       scroll: true,
@@ -151,44 +178,67 @@ function initSortData()
   });
 }
 
-function initCheckAll()
-{
-  $("table").each(function () {
-    if ($(".check-master").length > 0) {
-      $(".check-master").each(function () {
-        $(this).on("change", function () {
-          let checked = $(this)[0].checked;
-          let selector = $(this).attr('data-selector');
-          let table = $(this).closest("table");
-          table.find(".check-slave").filter(selector).each(function () {
-            $(this)[0].checked = checked;
-          });
+/**
+ * Initializes the "Check All" functionality for tables with checkboxes.
+ * - Selects or deselects all checkboxes in a table when the master checkbox is toggled.
+ */
+function initCheckAll() {
+  // Find all tables
+  document.querySelectorAll("table").forEach(function(table) {
+    // Check if there's an element with the class "check-master" in the table
+    const masterCheckbox = table.querySelector(".check-master");
+    
+    if (masterCheckbox) {
+      masterCheckbox.addEventListener("change", function() {
+        const checked = masterCheckbox.checked;
+        const selector = masterCheckbox.getAttribute('data-selector');
+        
+        // Find all checkboxes in the table matching the selector
+        table.querySelectorAll(".check-slave" + selector).forEach(function(slaveCheckbox) {
+          slaveCheckbox.checked = checked;
         });
       });
     }
   });
 }
 
-function initAjaxSupport()
-{
-  if ($('[data-ajax-support="true"]').length == 0) {
-    $(document).on("click", '[type="submit"][name="user_action"][value="delete"]', function (e2) {
-        if(!confirm($(this).attr("data-onclik-message")))
-        {
+/**
+ * Initializes AJAX support for form submissions.
+ * - Attaches a confirmation prompt for delete actions if the element has data-ajax-support="true".
+ */
+function initAjaxSupport() {
+  // Check if there are elements with the attribute data-ajax-support="true"
+  if (document.querySelectorAll('[data-ajax-support="true"]').length === 0) {
+    // Attach a click event listener to the submit buttons with specific attributes
+    document.addEventListener("click", function(e2) {
+      const target = e2.target;
+
+      if (target && target.type === "submit" && target.name === "user_action" && target.value === "delete") {
+        // Check for confirmation before proceeding with the action
+        const confirmationMessage = target.getAttribute("data-onclik-message");
+        if (!confirm(confirmationMessage)) {
           e2.preventDefault();
           e2.stopPropagation();
         }
       }
-    );
+    });
   }
 }
 
+/**
+ * Initializes multiple select dropdowns using the MultiSelect library.
+ * - Allows for selecting multiple options in a dropdown.
+ */
 function initMultipleSelect()
 {
   document.querySelectorAll('select[multiple]').forEach(select => new MultiSelect(select));
 }
 
-
+/**
+ * Initializes hidden input fields for sorting parameters in the URL.
+ * - Adds hidden inputs to the form to reflect the current sort order and column.
+ * @param {string} queryString The query string from the current URL.
+ */
 function initOrderUrl(queryString)
 {
   const urlParams = new URLSearchParams(queryString);
@@ -223,7 +273,11 @@ function initOrderUrl(queryString)
   }
 }
 
-
+/**
+ * Updates the order numbers for sorted rows in a form.
+ * - Adds new order values as hidden inputs for each row.
+ * @param {object} dataToSort The table or section being sorted.
+ */
 function updateNumber(dataToSort) {
   let frm = dataToSort.closest("form");
   if (frm.find("span.new-sort-order").length) {
@@ -253,7 +307,22 @@ function updateNumber(dataToSort) {
     .find('button[name="user_action"][value="sort_order"]')
     .removeAttr("disabled");
 }
-function saveOrder() {}
+
+/**
+ * Placeholder for the saveOrder function.
+ * This function can be implemented for saving the sorted order to the server.
+ */
+function saveOrder() {
+  // Do nothing
+}
+
+/**
+ * Splits a string by a delimiter and returns an array, keeping the remaining part as a tail.
+ * @param {string} str The string to split.
+ * @param {string} delimiter The delimiter to split by.
+ * @param {number} count The number of parts to return before the tail.
+ * @returns {Array} The array of split parts, with the remaining part as a tail.
+ */
 function splitWithTail(str, delimiter, count) {
   const parts = str.split(delimiter);
   const tail = parts.slice(count).join(delimiter);
@@ -261,7 +330,70 @@ function splitWithTail(str, delimiter, count) {
   result.push(tail);
   return result;
 }
-jQuery(function ($) {
+
+
+/**
+ * Initializes notifications in the dropdown menu.
+ * @param {Array} notifications Array of notification objects containing message, time, and link.
+ */
+function initNotifications(notifications) {
+  const notificationMenu = document.getElementById('notificationMenu');
+  notifications.forEach(notification => {
+      const a = document.createElement('a');
+      a.className = 'dropdown-item';
+      a.href = notification.link;
+      a.innerHTML = `${notification.message} <small class="text-muted">${notification.time}</small>`;
+      a.dataset.id = notification.id; // Adding ID to the notification item
+      notificationMenu.appendChild(a);
+  });
+}
+
+/**
+ * Initializes messages in the dropdown menu.
+ * @param {Array} messages Array of message objects containing message, time, and link.
+ */
+function initMessages(messages) {
+  const messageMenu = document.getElementById('messageMenu');
+  messages.forEach(message => {
+      const a = document.createElement('a');
+      a.className = 'dropdown-item';
+      a.href = message.link;
+      a.innerHTML = `${message.message} <small class="text-muted">${message.time}</small>`;
+      a.dataset.id = message.id; // Adding ID to the message item
+      messageMenu.appendChild(a);
+  });
+}
+
+/**
+ * Initializes the page by setting up event listeners for sidebar toggle, dark/light mode toggle, 
+ * and initializing other functions like notifications, messages, and form actions.
+ */
+function initPage()
+{
+  // Toggle sidebar visibility
+  // Select all elements with the class .toggle-sidebar
+  document.querySelectorAll('.toggle-sidebar').forEach(toggleButton => {
+    toggleButton.addEventListener('click', () => {
+        let width = document.body.clientWidth;
+        if (width >= 992) {
+            document.body.classList.toggle('sidebar-hidden'); // Hide or show the sidebar for large screens
+        } else {
+            document.body.classList.toggle('sidebar-show'); // Hide or show the sidebar for small screens
+        }
+    });
+  });
+
+  // Toggle between light and dark modes
+  document.querySelector('.toggle-mode').addEventListener('click', () => {
+      document.body.classList.toggle('dark-mode'); // Switch to dark mode
+      document.body.classList.toggle('light-mode'); // Switch to light mode
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  initPage();
+  initNotifications(notifications);
+  initMessages(messages);
   initCheckAll();
   initAjaxSupport();
   initDatetimePicker();
